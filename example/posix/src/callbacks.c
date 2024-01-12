3e612f54cd990a8c67cfbecbba0c3a1ec01c06c8
flaz83 <flavio.ferrandi@gmail.com>
2024-01-12 14:26:45 +0100

    fix: imports in test

--- example/posix/src/callbacks.c
+++ example/posix/src/callbacks.c
@@ -21,6 +21,8 @@
 
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <errno.h>
 #include <sys/time.h>
--- test/posix/cloud_functions.c
+++ test/posix/cloud_functions.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include <trackle_interface.h>
 
